2d6e87e81f96c8c902062eb5ee75746a7a81ee60
Ryan Zec <code@ryanzec.com>
2013-07-01 19:58:39 -0400

    Added broadcasting hooks

--- auto-focus.js
+++ auto-focus.js
@@ -1,13 +1,28 @@
 angular.module('nag.autoFocus', [])
 .directive('nagAutoFocus', [
   '$timeout',
-  function($timeout) {
+  '$rootScope',
+  function($timeout, $rootScope) {
     return {
       restrict: 'A',
+      controller: [
+        '$scope',
+        function($scope) {
+          $scope.$on('$destroy', function() {
+            //if the scope is destroyed, we no longer need this broadcast to be registered
+            $scope.unregisterBroadcast();
+          });
+        }
+      ],
       compile: function(element, attributes, transclude) {
         return {
           post: function(scope, element, attributes) {
             $timeout(function(){$(element).focus()}, 0);
+
+            //allows you to trigger the auto focus through javascript code without having to reply of DOM selection (like inside a controller)
+            scope.unregisterBroadcast = scope.$on('trigger-auto-focus', function() {
+                $(element).focus()
+            });
           }
         };
       }
