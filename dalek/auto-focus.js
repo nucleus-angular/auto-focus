d4a7a3b3840b800bb47778bb75b8925cf68ab060
Ryan Zec <code@ryanzec.com>
2014-03-05 05:55:09 -0500

    updated tests updated libraries

--- dalek/auto-focus.js
+++ dalek/auto-focus.js
@@ -13,17 +13,16 @@ module.exports = {
     .wait(500)
       .assert.exists('.not-focused', 'element not focused')
     .done();
-  }//,
+  },
 
-
-  /*'should auto focus input when event is triggered': function(test) {
+  'should auto focus input when event is triggered': function(test) {
     test.open('http://localhost:3000/home/2')
     .wait(500)
     .click('body')
       .assert.exists('.not-focused', 'element not focused')
     .click('[data-ut="focus-input"]')
     .wait(500)
-      .assert.exists('.is-focused', 'element focused')
+        .assert.exists('.is-focused', 'element focused')
     .done();
-  }*/
+  }
 }
